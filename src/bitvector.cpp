c7ac410dc595fcdc91c272a8980902d76f3ca726
Joshua Hauth <git@jhauth.de>
2024-06-25 14:22:33 +0200

    feat(Bitvector): init class

---
+++ src/bitvector.cpp
@@ -0,0 +1,16 @@
+#include "bitvector.hpp"
+
+Bitvector::Bitvector(std::string bits)
+: bitvector(bits.size()) {}
+
+bool Bitvector::access(size_t pos) {
+    return 0;
+}
+
+size_t Bitvector::rank(bool bit, size_t i) {
+    return 0;
+}
+
+size_t Bitvector::select(bool bit, size_t i) {
+    return 0;
+}
---
+++ src/bitvector.hpp
@@ -0,0 +1,42 @@
+#ifndef BITVECTOR_BITVECTOR_HPP
+#define BITVECTOR_BITVECTOR_HPP
+
+#include <cstddef>
+#include <cstdint>
+#include <vector>
+#include <string>
+
+class Bitvector {
+
+public:
+    explicit Bitvector(std::string bits);
+
+    /**
+     * Access the bit a specific position
+     * @param pos The position to access
+     * @return The bit at pos as bool
+     */
+    bool access(size_t pos);
+
+    /**
+     * Get the position with i zeros or ones before
+     * @param bit What bit to track
+     * @param i Amount of bits before position
+     * @return The position where i bits are before
+     */
+    size_t select(bool bit, size_t i);
+
+    /**
+     * Get the number of bits bit before position i
+     * @param bit What bit to track
+     * @param i The position to begin tracking
+     * @return Number of bits of type bit before the position i
+     */
+    size_t rank(bool bit, size_t i);
+
+private:
+    std::vector<uint64_t> bitvector;
+};
+
+
+#endif //BITVECTOR_BITVECTOR_HPP
